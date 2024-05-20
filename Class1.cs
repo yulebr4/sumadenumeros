345802e41b0a7258a6c3c436723ea3635b585a60
MOISES <MOISES@DESKTOP-17FT0L7>
2024-05-19 23:55:57 -0400

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace sumadenumeros
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\n"Este programa es para sumar numeros\n");
+            int uno, dos, resultado;
+
+            Console.WriteLine("Ingrese el primer numero a sumar: \n");
+            uno = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero a sumar: \n");
+            dos = int.Parse(Console.ReadLine());
+
+            resultado = uno + dos;
+
+            Console.WriteLine("La suma de los numeros ingresados es:  \n" + resultado);
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
